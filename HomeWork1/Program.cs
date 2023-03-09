14adc75733b275f057d92d7224ba7f8bbf9eb7d1
Egan Dudin <eganddn@yandex.ru>
2023-03-09 17:23:31 +0700

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+    namespace HelloWorld
+{
+    class program
+
+    { static void Main(string[] args)
+        {
+            Console.Write("Hello, BeeGeek!"); 
+        }
+    }
+}
