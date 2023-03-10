4f6ea707f5687236384e0a35d380376ea66218ea
Egan Dudin <eganddn@yandex.ru>
2023-03-10 11:49:37 +0700

    test2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-    namespace HelloWorld
+using System.Security.Cryptography.X509Certificates;
+
+namespace HelloWorld
 {
     class program
-
-    { static void Main(string[] args)
+           {
+        static void Main(string[] args)
         {
-            Console.WriteLine("Строка 1");
-            Console.WriteLine("");
-            Console.WriteLine("Строка 2");
+            int a = 5;
+            int b = 10;
+            Console.WriteLine(a + b);
 
-                
+            string s1 = "5";
+            string s2 = "10";
+            Console.WriteLine(s1 + s2);
+
 
         }
     }
