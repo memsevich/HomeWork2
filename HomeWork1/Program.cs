a8751f7deb2ee98e7c42816b8df3572974ea2b52
Egan Dudin <eganddn@yandex.ru>
2023-03-13 16:23:52 +0700

    TestConvert

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -7,16 +7,23 @@ namespace HelloWorld
            {
         static void Main(string[] args)
         {
-            
-            string name1 = Console.ReadLine();
-            string name2 = Console.ReadLine(); 
-            string name3 = Console.ReadLine();
-          
-            Console.WriteLine( name1 );
-            Console.WriteLine( name2 );
-            Console.WriteLine( name3 );
-          
 
+            ///string name1 = Console.ReadLine();
+            ///string name2 = Console.ReadLine(); 
+            ///string name3 = Console.ReadLine();
+
+            ///Console.WriteLine( name1 );
+            ///Console.WriteLine( name2 );
+            ///Console.WriteLine( name3 );
+            ///int num1 = Convert.ToInt32 (Console.ReadLine());
+            ///int num2 = Convert.ToInt32 (Console.ReadLine());
+            ///Console.WriteLine(num1 + num2);
+
+            int a = 5;
+            int b = 2;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
 
         }
     }
